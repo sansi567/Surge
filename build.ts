d80fda640080f416ec23c3b543cfe07cc2fd9db6
Aoang <aoang@x2oe.com>
2023-05-09 19:48:57 +0800

    update build

--- build.ts
+++ build.ts
@@ -2,9 +2,11 @@ import * as build from "./src/mod.ts";
 
 console.log(`Build start`);
 
-Object.entries(build).map(async ([key, val]) => {
-  console.log(`Build: ${key}...`);
-  await val();
-});
+await Promise.all(
+  Object.entries(build).map(async ([key, val]) => {
+    console.log(`Build: ${key}...`);
+    await val();
+  }),
+);
 
 console.log(`Build finished`);
