585ce96146222a12e33f7fced336180984d3cb65
Aoang <aoang@x2oe.com>
2023-11-14 16:03:27 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -3,7 +3,7 @@ import { serveDir } from "https://deno.land/std@0.186.0/http/file_server.ts";
 
 await serve((req) => {
   const pathname = new URL(req.url).pathname;
-  if (pathname.startsWith("/Modules") || pathname.startsWith("/Ruleset")) {
+  if (pathname.startsWith("/Modules") || pathname.startsWith("/Ruleset") || pathname.startsWith("/Script")) {
     return serveDir(req);
   }
 
